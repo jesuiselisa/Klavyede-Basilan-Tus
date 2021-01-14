f4131000fb1e372edb1c3bbc13995f860a365d9f
jesuiselisa <75565584+jesuiselisa@users.noreply.github.com>
2021-01-15 00:01:42 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace BasilanTus
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblBasilanTus = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblBasilanTus
+            // 
+            this.lblBasilanTus.AutoSize = true;
+            this.lblBasilanTus.BackColor = System.Drawing.Color.Linen;
+            this.lblBasilanTus.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblBasilanTus.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblBasilanTus.Location = new System.Drawing.Point(163, 136);
+            this.lblBasilanTus.Name = "lblBasilanTus";
+            this.lblBasilanTus.Size = new System.Drawing.Size(71, 31);
+            this.lblBasilanTus.TabIndex = 0;
+            this.lblBasilanTus.Text = "YOK";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Linen;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label2.Location = new System.Drawing.Point(103, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(192, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Bastığınız Tuş:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SeaGreen;
+            this.ClientSize = new System.Drawing.Size(397, 252);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblBasilanTus);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Keyboard";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblBasilanTus;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BasilanTus
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Escape)
+            {
+                lblBasilanTus.Text = "Esc";
+            }
+            else if (e.KeyChar == (char)Keys.Enter)
+            {
+                lblBasilanTus.Text = "Enter";
+            }
+            else if (e.KeyChar == (char)Keys.Space)
+            {
+                lblBasilanTus.Text = "Space";
+            }
+            else if (e.KeyChar == (char)Keys.Tab)
+            {
+                lblBasilanTus.Text = "Tab";
+            }
+            else
+            {
+                lblBasilanTus.Text = e.KeyChar.ToString();
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Down || e.KeyCode == Keys.Up || e.KeyCode == Keys.Left || e.KeyCode == Keys.Right)
+            {
+                lblBasilanTus.Text = e.KeyCode.ToString();
+            }
+        }
+    }
+}
